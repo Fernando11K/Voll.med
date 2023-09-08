150b8eb3bc0720ba91fbaffd8b9c211d113f8e2f
fernando11000 <fernandoferreira10@hotmail.com.br>
2023-09-07 23:42:46 -0300

    Boas práticas

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.sql.SQLOutput;
 import java.util.List;
@@ -21,55 +23,37 @@ public class MedicoController {
     private MedicoRepository repository;
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
-    repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder){
+        var medico = new Medico(dados);
+        repository.save(medico);
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
 
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
-//    @GetMapping
-//    public Page<DadosListagemMedico> listar(Pageable paginacao){
-//        return repository.findAll(paginacao).map(DadosListagemMedico::new);
-//    }
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(page);
     }
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico)); //Devolve 200
     }
-//    @DeleteMapping("/{id}")
-//    @Transactional
-//    public void excluir(@PathVariable Long id){
-//        repository.deleteById(id);
-//
-//    }
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id){
+    public ResponseEntity excluir(@PathVariable Long id){
         var medico = repository.getReferenceById(id);
         medico.excluir();
-
+        return ResponseEntity.noContent().build(); //Devolve código 204
     }
-//    @PostMapping("/lote")
-//    @Transactional
-//    public void cadastrarEmLote(@RequestBody List<DadosCadastroMedico> dados){
-//        dados.stream()
-//                .collect(Collectors.groupingBy(DadosCadastroMedico::especialidade))
-//                .forEach((key, value) -> {
-//                    String resultado = key +":" +value;
-//                    System.out.println(resultado);
-//                });
-//
-//    }
+     @GetMapping("/{id}")
+        public ResponseEntity detalhar(@PathVariable Long id){
+            var medico = repository.getReferenceById(id);
+            return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+        }
 
-    @PostMapping("/lote")
-    @Transactional
-    public void cadastrarEmLote(@RequestBody @Valid List<DadosCadastroMedico> dados){
-        dados.forEach(medico ->{
-            repository.save(new Medico(medico));
-        });
-
-    }
 }
--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -25,17 +25,34 @@ public class Paciente {
     private String email;
     private String cpf;
     private String telefone;
+    private Boolean ativo;
 
     @Embedded
     private Endereco endereco;
 
     public Paciente (DadosCadastroPaciente dados){
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
     }
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
 
 
+    public void excluir() {
+        this.ativo = false;
+    }
 }
